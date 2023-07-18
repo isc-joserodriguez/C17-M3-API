5fa1c4cc9d73594085f060b97fe9a901bec0909b
Jose Rodriguez <j.rodriguez@blackstone.studio>
2023-07-17 19:40:24 -0700

    Create & Read

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //! 1.- Importar express
 const express = require('express');
+const users = [];
 //! 2.- Instanciar express
 const app = express();
 //! 3.- Funciones ""especiales""" - Middlewares
@@ -8,6 +9,39 @@ app.use(express.json());
 app.get('/', (request, response) => {
   response.json({
     mensaje: 'Hola, esta es la API C17!!!',
+  });
+});
+
+app.post('/users', (request, response) => {
+  const { nombre, edad, correo, password, telefono, apellido, otra } =
+    request.body;
+  users.push({
+    nombre,
+    edad,
+    correo,
+    password,
+    telefono,
+    apellido,
+    otra,
+  });
+  response.json({
+    mensaje: 'Usuario guardado ',
+    data: users,
+  });
+});
+
+app.get('/users', (request, response) => {
+  response.json({
+    mensaje: 'Obtener todos los usuarios',
+    data: users,
+  });
+});
+
+app.get('/users/:id', (request, response) => {
+  const { id } = request.params;
+  response.json({
+    mensaje: 'Obtener un usuario',
+    data: users[Number(id)],
   });
 });
 
